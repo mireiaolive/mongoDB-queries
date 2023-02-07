d763676c8c6d58c1cc3a3f060834063b8ea0a014
Mireia Olivé <mirisfu@gmail.com>
2023-02-07 12:24:50 +0100

    first commit

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ db.restaurants.find( { borough: { $eq: "Bronx" } } ).sort({_id:-1}).limit(5);
 db.restaurants.find( { borough: { $eq: "Bronx" } } ).skip(5);
 db.restaurants.find( { "grades.score":{$gt:90}  } );
 db.restaurants.find( { "grades.score":{$gt:80, $lt:100}  } );
+db.restaurants.find( { "address.coord":{$lt:-95.754168}  } );
+
 
 
 
